e5a5a216cfb917e26949252faf0f4f87e3249d06
Michael Collins <mfcollins3@me.com>
2024-08-14 07:52:32 -0700

    feat: create secret key for hs256
    I implemented the ability to generate the secret cryptographic key used
    by the HMAC-SHA256 algorithm to sign a JSON web token. The cryptographic
    key is generated using a cryptographically secure random number
    generator and output to standard output or a file as a hex string.

--- src/main.rs
+++ src/main.rs
@@ -18,6 +18,41 @@
 // FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
 // IN THE SOFTWARE.
 
-fn main() {
-    println!("Hello, world!");
+use std::path::PathBuf;
+
+use clap::Parser;
+use rand::Rng;
+
+#[derive(Parser)]
+#[command(name = "JWT Developer Tools")]
+#[command(version = "0.1.0")]
+#[command(about = "Tools to help developers generate and verify JSON web tokens")]
+#[command(long_about = "\
+JSON Developer Tools is a set of commands that help developers to build
+distributed solutions that rely on JSON web tokens for identity and
+authorization. Developers can use JSON Developer Tools to generate the
+cryptographic keys that are used to sign and verify JSON web tokens, generate
+a JSON web token for debugging and testing, verify a JSON web token, or to
+view the contents of a JSON web token.")]
+struct ProgramArgs {
+    /// The path where the secret key should be output
+    #[arg(short, long)]
+    output: Option<PathBuf>,
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let args = ProgramArgs::parse();
+
+    let mut writer: Box<dyn std::io::Write> = match args.output {
+        Some(path) => Box::new(std::fs::File::create(path).expect("Unable to create file")),
+        None => Box::new(std::io::stdout()),
+    };
+    let mut rng = rand::thread_rng();
+    let key = (0..32).map(|_| rng.gen::<u8>()).collect::<Vec<u8>>();
+    for byte in key {
+        write!(writer, "{:02x}", byte)?;
+    }
+    
+    writeln!(writer)?;
+    Ok(())
 }
---
+++ tests/hs256.rs
@@ -0,0 +1,50 @@
+// Copyright 2024 Michael F. Collins, III
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to
+// deal in the Software without restriction, including without limitation the
+// rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+// sell copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+// IN THE SOFTWARE.
+
+use assert_cmd::prelude::*;
+use predicates::prelude::*;
+use std::error::Error;
+use std::io::Read;
+use std::process::Command;
+
+#[test]
+fn hs256_secret_key_is_64_hex_characters() -> Result<(), Box<dyn Error>> {
+    let mut cmd = Command::cargo_bin("jwt")?;
+    cmd.assert()
+        .success()
+        .stdout(predicate::str::is_match("^[0-9a-fA-F]{64}")?);
+    Ok(())
+}
+
+#[test]
+fn hs256_secret_key_is_written_to_file() -> Result<(), Box<dyn Error>> {
+    let mut cmd = Command::cargo_bin("jwt")?;
+    let temp_file = tempfile::NamedTempFile::new()?;
+    let temp_file_path = temp_file.into_temp_path();
+    cmd.arg("--output").arg(temp_file_path.to_path_buf());
+    cmd.assert().success();
+    let mut file = std::fs::File::open(temp_file_path)?;
+    let mut key = String::new();
+    let length = file.read_to_string(&mut key)?;
+    assert_eq!(length, 65);
+    let re = regex::Regex::new("^[0-9a-fA-F]{64}\\n$")?;
+    assert!(re.is_match(key.as_str()), "The key is not a valid hexadecimal string");
+    Ok(())
+}
